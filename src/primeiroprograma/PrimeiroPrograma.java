4026c629cd3b8accae03bd359b5d43a035298086
Guinter Zaffalon <guinterzaffalon@MacBook-Air-de-Guinter.local>
2024-05-21 09:21:25 -0300

    primeiro commit

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package primeiroprograma;
+/**
+ *
+ * @author guinterzaffalon
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
