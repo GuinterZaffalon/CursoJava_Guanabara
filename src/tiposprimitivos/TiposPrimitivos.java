f91699aa60832fbb8c8421a201822dbb2f46a9ec
Guinter Zaffalon <guinterzaffalon@MacBook-Air-de-Guinter.local>
2024-05-22 09:19:34 -0300

    att

--- src/tiposprimitivos/TiposPrimitivos.java
+++ src/tiposprimitivos/TiposPrimitivos.java
@@ -4,6 +4,7 @@ package tiposprimitivos;
 public class TiposPrimitivos {
     public static void main(String[] args) {
         float nota = 8.5f;
-        System.out.println("A nota é:" +nota);
+        String nome = "João Lemos";
+        System.out.printf("A nota de %s é %.2f ", nome, nota );
     }
 }
