ec75d82312466373ef74c1e385bae7a733f5958f
Guinter Zaffalon <guinterzaffalon@MacBook-Air-de-Guinter.local>
2024-05-22 09:16:04 -0300

    finalização de tipos em terminal

---
+++ src/tiposprimitivos/TesteTipos.java
@@ -0,0 +1,16 @@
+
+package tiposprimitivos;
+
+
+public class TesteTipos {
+    public static void main(String[] args) {
+        
+        /*int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println("");*/
+        
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.println(idade);
+    }
+}
