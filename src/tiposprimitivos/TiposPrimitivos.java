6249c7dfae93c9ca94ae5cbd6b1a7905fd9ce215
Guinter Zaffalon <guinterzaffalon@MacBook-Air-de-Guinter.local>
2024-05-22 08:37:28 -0300

    adição dos primitivos/1

---
+++ src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package tiposprimitivos;
+
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        System.out.println("A nota é:" +nota);
+    }
+}
