0fe50da0fbf0884f4fc858f58266a62ef37eaf9d
Guinter Zaffalon <guinterzaffalon@192.168.2.105>
2024-05-24 10:27:40 -0300

    posso votar?

---
+++ src/podevotar/PodeVotar.java
@@ -0,0 +1,27 @@
+
+package podevotar;
+import java.util.Scanner;
+
+
+public class PodeVotar {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Digite seu ano de nascimento: ");
+        int nascimentoAno = teclado.nextInt();
+        
+        int calculoIdade = 2024 - nascimentoAno;
+        System.out.println("Sua idade é: " +calculoIdade);
+        
+        if (calculoIdade < 16) {
+            System.out.println("Não pode votar!");
+        } else  {
+            if ((calculoIdade > 16 && calculoIdade < 18) || (calculoIdade > 70)){
+                System.out.println("Voto opcional!");
+            }
+            else{
+                System.out.println("Voto obrigatório!");
+            }
+        }
+    }
+}
